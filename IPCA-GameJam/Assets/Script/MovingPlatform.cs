ee5bbf1b9bfdf45a5871e29bcb5aca432af9dc6c
barreiro.pedro7@gmail.com <barreiro.pedro7@gmail.com>
2017-03-31 23:09:54 +0100

    O Whisp é gay

--- IPCA-GameJam/Assets/Script/MovingPlatform.cs
+++ IPCA-GameJam/Assets/Script/MovingPlatform.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour {
 
-	public Rigidbody2D rigidBody;
+	private Rigidbody2D rigidBody;
+	public GameObject gameObject;
 
-	// Use this for initialization
 	void Start () {
-		
+		gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 0));
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.name.Equals ("fundo")) {
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-200, 0));
+			Debug.Log ("#triggerd");
+	}
+		else if (collision.gameObject.name.Equals("fundo1"))
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(200, 0));
+	}
+
 }
