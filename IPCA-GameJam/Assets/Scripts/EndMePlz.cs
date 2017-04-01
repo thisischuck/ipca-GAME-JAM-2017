63024376554f275a5c4275002de56d9e3e03d04c
Unknown <josemachado.gamedesign@gmail.com>
2017-04-01 23:27:55 +0100

    git gud, scrub

---
+++ IPCA-GameJam/Assets/Scripts/EndMePlz.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EndMePlz : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+            if (Input.GetKey(KeyCode.E))
+                Application.Quit();
+    }
+}
---
+++ IPCA-GameJam/Assets/Scripts/Glasses.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glasses : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject sign_desfocado;
+    public GameObject sign_focado;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+            {
+                player.GetComponent<Player>().hasGlasses = true;
+                gameObject.SetActive(false);
+            sign_desfocado.SetActive(false);
+            sign_focado.SetActive(true);
+
+            }
+    }
+}
