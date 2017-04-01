54ff99863af8c819dcb3de7c8243e8212d9b01d1
Unknown <brunofreitas5.25@hotmail.com>
2017-04-01 19:18:40 +0100

    O Xpop é gay v6

---
+++ IPCA-GameJam/Assets/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    private Player player;
+
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Death"))
+        {
+            player.death = true;
+        }
+    }
+}
