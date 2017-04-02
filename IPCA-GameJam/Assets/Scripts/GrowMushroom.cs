e958709a3b5c6f02e0a72831daa009c52682ed77
Carlos Costa <washinima99@gmail.com>
2017-04-02 15:12:06 +0100

    Bug fixes + spike fall

--- IPCA-GameJam/Assets/Scripts/GrowMushroom.cs
+++ IPCA-GameJam/Assets/Scripts/GrowMushroom.cs
@@ -9,35 +9,41 @@ public class GrowMushroom : MonoBehaviour {
     public GameObject water;
     public GameObject ice;
 
+    private Rigidbody2D rigidbody;
+
     private int aux;
 
     // Use this for initialization
     void Start () {
+
         aux = speed;
         ice.SetActive(false);
+        rigidbody = this.GetComponentInParent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
         if (player.GetComponent<Player>().weather == 1)
-        {
+        { 
             transform.Translate(0, speed * Time.deltaTime, 0);
             water.SetActive(true);
             ice.SetActive(false);
         }
         else
         {
-            speed = aux;
             transform.Translate(0, -speed * Time.deltaTime, 0);
             water.SetActive(false);
             ice.SetActive(true);
-        }
-	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Collider" || collision.gameObject.tag == "Ground")
-        speed = 0;
+            aux = 0;
     }
 
 }
