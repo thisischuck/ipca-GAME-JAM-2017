3af5c3b221c189c8a7151fcf62d5e70c23474819
Unknown <josemachado.gamedesign@gmail.com>
2017-04-02 01:38:17 +0100

    menus and all

---
+++ IPCA-GameJam/Assets/Scripts/MenuAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimation : MonoBehaviour {
+
+    public GameObject other;
+
+    private int cont;
+    private bool state;
+
+
+	// Use this for initialization
+	void Start () {
+        cont = 30;
+        state = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(cont <= 0)
+        {
+            cont = 30;
+            other.SetActive(!other.activeInHierarchy);
+
+        }
+        else
+        {
+            cont--;
+        }
+    }
+}
---
+++ IPCA-GameJam/Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
